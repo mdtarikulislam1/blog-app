afeda0a199ee5c37c40e98e8a07090a4132dd888
md torikul <mdtorikulislamprogrammer@gmail.com>
2025-12-27 22:25:46 +0600

    create post

---
+++ src/modules/post/post.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { postService } from "./post.service";
+
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const result = await postService.createPost(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(401).json({
+      error: "post creation faild",
+      details: error,
+    });
+  }
+};
+
+export const PostControler = {
+  createPost,
+};
